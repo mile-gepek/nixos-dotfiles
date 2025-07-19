756ff5b76c84f3d37ff84402442d2be5ff263939
mile gepek <theontley@gmail.com>
2025-07-19 15:42:03 +0200

    Fixed ligatures not working in ghostty.

--- modules/home-manager/ghostty/ghostty.nix
+++ modules/home-manager/ghostty/ghostty.nix
@@ -18,8 +18,7 @@ in
         font-style-bold = "\"Bold\"";
         font-style-italic = "\"SemiBold Italic\"";
         font-style-bold-italic = "\"Bold Italic\"";
-        font-feature = "+calt +liga +ss01 +ss02 +ss03 +ss04 +ss05 +ss06 +ss07 +ss09 +ss10";
-        
+        font-feature = ["+calt" "+liga" "+ss01" "+ss02" "+ss03" "+ss04" "+ss05" "+ss06" "+ss07" "+ss09" "+ss10"];
         font-size = 15;
       
         adjust-cell-height = "20%";
